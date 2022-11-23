de08ea98a147e249344d107bfb428e737ec865ca
Isa Memmedli <i.a.memmedli@gmail.com>
2022-11-24 01:48:01 +0400

    yoruldum

---
+++ TechPortalServer/Migrations/20221123214646_sss.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechPortalServer.Migrations
+{
+    public partial class sss : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Department",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Faculty",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Faculty",
+                table: "Users");
+        }
+    }
+}
